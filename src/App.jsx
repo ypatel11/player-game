f7991b1bc5dea13adbf551055791bf9f581612d2
ypatel11 <ybpatel117@gmail.com>
2025-05-13 15:38:06 +0530

    timmer start

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
-      <TimerChallenge/>
+      <div id="challenges">
+        <TimerChallenge title='Easy' targetTime={1}/>
+        <TimerChallenge title='Medium' targetTime={5}/>
+        <TimerChallenge title='Tough' targetTime={10}/>
+        <TimerChallenge title='Pros only' targetTime={15}/>
+      </div>
     </>
   );
 }
